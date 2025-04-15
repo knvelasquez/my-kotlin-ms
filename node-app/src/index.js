349e9ef44aa18e899a910ff92dec3dfc6c35ab1a
knvelasquez <knvelasquez@hotmail.com>
2025-04-15 19:22:08 +0000

    fix

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 ¡Mi app Node.js está corriendo!');
+  res.send('🚀 ¡Mi app Node.js está corriendo! yay');
 });
 
 app.listen(port, () => {
